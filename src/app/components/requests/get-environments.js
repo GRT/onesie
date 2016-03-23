11bd9227276358ded467d1c794ffdd84e4930f87
Oakley Hall <ohall@walmart.com>
2016-03-23 09:37:47 -0700

    get environments module

--- src/app/components/requests/get-environments.js
+++ src/app/components/requests/get-environments.js
@@ -1,3 +1,15 @@
 /**
  * Created by Oakley Hall on 3/23/16.
  */
+
+/**
+ * @param error - error callback
+ *
+ * @param callback  - error callback
+ */
+export default function (error, callback) {
+  callback(['DEV',
+    'PROD',
+    'QA',
+    'STAGE']);
+}
