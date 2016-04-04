5deb1cad99264a7faffb47e4843e5bd99e13e72d
Oakley Hall <ohall@walmart.com>
2016-04-04 09:18:24 -0700

    - SINGLE QUOTES!

--- src/app/app.js
+++ src/app/app.js
@@ -1,7 +1,7 @@
-"use strict";
-require("./components/style.css");
-import ScrollArea from "./components/scroll.jsx";
+'use strict';
+require('./components/style.css');
+import ScrollArea from './components/scroll.jsx';
 import ReactDOM from 'react-dom';
 import React from 'react';
 
-ReactDOM.render(React.createElement(ScrollArea, null), document.getElementById("main"));
+ReactDOM.render(React.createElement(ScrollArea, null), document.getElementById('main'));
