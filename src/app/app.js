a4859ec46c2bd2a06aa623471b6c7d2af7254cb9
Oakley Hall <ohall@walmart.com>
2016-04-06 07:05:26 -0700

    updating

--- src/app/app.js
+++ src/app/app.js
@@ -4,9 +4,15 @@ import ScrollArea from "./components/scroll.jsx";
 import ReactDOM from 'react-dom';
 import React from 'react';
 import ips from './components/requests/get-ips';
+import orgs from './components/requests/get-orgs';
 
 ReactDOM.render(React.createElement(ScrollArea, null), document.getElementById("main"));
 
 ips(null, null, (iparray) => {
   console.log('IPs: ' + iparray );
 });
+
+orgs(null, (orgs) => {
+  console.log('orgs: ' + orgs );
+});
+
