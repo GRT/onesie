121142c44ec6af0188323584c254f4db768d811c
Oakley Hall <ohall@walmart.com>
2016-04-08 07:28:33 -0700

    basic req tests

---
+++ test/components/requests/get-environments-test.js
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import envs from '../../../src/app/components/requests/get-environments'
+
+describe('Environments request module', () => {
+  envs(null, (res) => {
+    it('should pass an array Environments to callback', () => {
+      expect(res).to.be.an('array');
+    });
+  });
+});
