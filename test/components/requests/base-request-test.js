51af52678b90325c77bc4ed57460268a0793ad3c
Oakley Hall <ohall@walmart.com>
2016-04-08 06:45:22 -0700

    base req test stub

---
+++ test/components/requests/base-request-test.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import { doGet } from '../../../src/app/components/requests/base-request'
+
+describe('Base request module', () => {
+  it('should make some kind of request', () => {
+    expect(1).to.equal(1);
+  });
+});
