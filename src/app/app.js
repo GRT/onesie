0d230d7df9a91252424d01203db961e338332393
Oakley Hall <ohall@walmart.com>
2016-04-27 10:56:15 -0700

    - test assemblies module

--- src/app/app.js
+++ src/app/app.js
@@ -5,14 +5,23 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import ips from './components/requests/get-ips';
 import orgs from './components/requests/get-orgs';
+import assems from './components/requests/get-assemblies';
 
 ReactDOM.render(React.createElement(ScrollArea, null), document.getElementById('main'));
 
-ips(null, null, (iparray) => {
+let err = function err(e) { console.error(e); };
+
+
+ips(err, null, (iparray) => {
   console.log('IPs: ' + iparray );
 });
 
-orgs(null, (orgs) => {
+orgs(err, (orgs) => {
   console.log('orgs: ' + orgs );
 });
+
+assems(err, { ooOrganization : 'CorpOrg'}, (assemblies) => {
+  console.log('Assemblies: ' + assemblies );
+});
+
 
