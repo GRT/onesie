6d9d5314dfb4a1e7261de9b7698fcbc359f9e057
Juan Carlos Garzon <juancarlosgarzon@Scott-Summers.local>
2016-06-30 22:18:46 -0400

    Fixed Linting Errors

--- src/app/reducers/organizations.jsx
+++ src/app/reducers/organizations.jsx
@@ -1,52 +1,50 @@
-import React from 'react';
+import _ from 'lodash';
 import update from 'react-addons-update';
 
 const namespace = 'organizations';
 
 function organizations(state = {} , action) {
 
-	switch(action.type){
-    
-		case namespace +'.SET.ORGS':
-      var orgObj = {};
+  switch(action.type){
+  case namespace +'.SET.ORGS':
+    var orgObj = {};
 
-      action.organizations.forEach(function(org, index) {
-        orgObj[org.name] = { assemblies: [] }
-      });
+    action.organizations.forEach(function(org) {
+      orgObj[org.name] = { assemblies: [] };
+    });
 
-			state = update(state, {items: {$set: orgObj }} );
-		break;
-
-		case namespace +'.SET.SELECTED':
-			state = update(state, {selected: {$set: action.selectedOrganization }} );
-		break;
-
-    case namespace +'.SET.ASSEMBLIES':
-      var updateObj = {items: {}};
-      updateObj.items[action.organization] = {$set: {assemblies: action.assemblies}};
-
-      state = update(state, updateObj);
+    state = update(state, {items: {$set: orgObj }} );
     break;
 
-    case namespace + ".SET.ENVS":
-      var updateObj = {items: {}};
-      var assems = state.items[action.organization].assemblies;
-
-      var updatedAssems = _.map( assems, function(item, index){
-        //TO Do: This should be changed to ciId, need to updated static data
-        if(item.ciName === action.assembly.ciName){
-          item.environments = action.environments;
-        }
-        return item;
-      });
-
-      updateObj.items[action.organization] = {assemblies: {$set: updatedAssems}};
-
-      state = update(state, updateObj);
+  case namespace +'.SET.SELECTED':
+    state = update(state, {selected: {$set: action.selectedOrganization }} );
     break;
-	}
 
-	return state
+  case namespace +'.SET.ASSEMBLIES':
+    var setOrgAssemsObj = {items: {}};
+    setOrgAssemsObj.items[action.organization] = {$set: {assemblies: action.assemblies}};
+    state = update(state, setOrgAssemsObj);
+    break;
+
+  case namespace + '.SET.ENVS':
+    var setAssemsEnvsObj = {items: {}};
+    var assems = state.items[action.organization].assemblies;
+
+    var updatedAssems = _.map( assems, function(item){
+      //TO Do: This should be changed to ciId, need to updated static data
+      if(item.ciName === action.assembly.ciName){
+        item.environments = action.environments;
+      }
+      return item;
+    });
+
+    setAssemsEnvsObj.items[action.organization] = {assemblies: {$set: updatedAssems}};
+
+    state = update(state, setAssemsEnvsObj);
+    break;
+  }
+
+  return state;
 }
 
 
