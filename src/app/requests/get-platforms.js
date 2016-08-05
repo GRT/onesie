c90b8017b12177a7f3034e0747329d1be33c1d18
Juan Carlos Garzon <juancarlosgarzon@Scott-Summers.local>
2016-08-05 15:16:30 -0400

    Get Platform API Request Wrapper Added

---
+++ src/app/requests/get-platforms.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Oakley Hall on 3/21/16.
+ */
+
+import {doGet} from './base-request';
+
+/**
+ * @param error - error callback
+ *
+ * @param params - parameter object. Ex:
+ *
+ * {
+ *  ooOrganization : 'SomeOrg',
+ * }
+ *
+ * @param callback  - callback
+ */
+export default function (error, params, callback) {
+  const path =  '/' + params.ooOrganization + '/assemblies/' +
+    params.ooAssembly + '/operations/environments/' + params.ooEnvironment + '/platforms.json' ;
+  doGet(error,path, assemblyArray => { callback(assemblyArray); });
+};
