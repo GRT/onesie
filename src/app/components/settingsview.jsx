18fae0be7ade55567f1a1e75866f1fea02d9f9a2
jaredmharris <jharris@grassroots-tech.com>
2016-08-25 16:23:00 -0400

    import react-bootstrap

--- src/app/components/settingsview.jsx
+++ src/app/components/settingsview.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from 'react-bootstrap/lib/Button';
 
 const SettingStyles = {
     //height: 'auto',
@@ -14,6 +15,7 @@ class SettingsView extends React.Component {
     render() {
         return (
           <div style={{float:'right'}}>
+            Hello
             <img src="src/static/gear.png" style={SettingStyles} />
           </div>
         );
@@ -21,3 +23,24 @@ class SettingsView extends React.Component {
 }
 
 export default SettingsView;
+
+class SettingsModal extends React.Component {
+  render () {
+    return (
+      <div>
+        <Modal>
+          <Modal.Header>
+            <Modal.Title>Settings</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Hello
+          </Modal.Body>
+          <Modal.Footer>
+            <Button>Close</Button>
+            <Button bsStyle="primary">Save</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    )
+  }
+}
