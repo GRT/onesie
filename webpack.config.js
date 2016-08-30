3b7c22ce5ad8b6c08a640c5bf103e97c340499cb
Troy Sampson <tjsampson11@gmail.com>
2016-08-30 13:20:35 -0600

    Added server.js file at root to wrap WebPack Dev Server configurations. Updated package.json npm start command to call node server.js instead of webpack dev server directly. Update webpack.config with React Hot Reloader.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
+var webpack = require('webpack');
+
 module.exports = {
   context: __dirname + '/src/app',
-  entry: {
-    javascript: './index.js',
-    html: './index.html'
-  },
+  entry: [
+  'webpack-dev-server/client?http://127.0.0.1:8080', // WebpackDevServer host and port
+  'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+  './index.js', // Your appʼs entry point,
+  './index.html'
+  ],
   output: {
     filename: 'app.js',
     path: __dirname + '/dist'
@@ -15,12 +19,16 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel-loader',
-        query: { presets: ['es2015', 'react'] }
+        test: /\.(js|jsx)$/, 
+        exclude: /node_modules/, 
+        loaders: ['react-hot', 'babel?presets[]=react,presets[]=es2015']
       },
       { test: /\.html$/, loader: 'file?name=[name].[ext]' },
       { test: /\.css$/, loader: 'style-loader!css-loader' },
       { test: /\.ico$/, loader: 'file-loader?name=[name].[ext]'}
     ]
-  }
+  },
+  plugins : [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
