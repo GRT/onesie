6b38f5b169010ab253aea7a08163dc509e5853eb
jaredmharris <jharris@grassroots-tech.com>
2016-09-07 09:51:21 -0400

    add details popup form

--- src/app/components/settingsbutton.jsx
+++ src/app/components/settingsbutton.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import Modal from 'react-bootstrap/lib/Modal';
+import ClusterToggleView from 'onesie-toggle-environment-block';
+import Button from 'react-bootstrap/lib/Button';
+import Col from 'react-bootstrap/lib/Col';
+import ControlLabel from 'react-bootstrap/lib/ControlLabel';
+import Form from 'react-bootstrap/lib/Form';
+import FormControl from 'react-bootstrap/lib/FormControl';
+import FormGroup from 'react-bootstrap/lib/FormGroup';
+
 
 const settingsStyles = {
   verticalAlign: 'middle',
@@ -8,45 +15,69 @@ const settingsStyles = {
   width: '25'
 }
 
+const thumbStyle = {
+  //margin: '.5em',
+  //padding: '.5em',
+  //width: '10em',
+  //display: 'flex',
+  //flexDirection: 'column',
+  //border: '1px solid white',
+  //borderRadius: '3px',
+  //boxShadow: '0 2px 2px rgba(0, 0, 0, .48)',
+  //overflow:'hidden',
+  minHeight: '3em',
+  width: '5em',
+  backgroundImage: 'url( src/static/gear.png )',
+  backgroundRepeat: 'no-repeat',
+  backgroundColor: '#f0f0f5'
+};
+
+const thumb = <div></div>;
+const formInstance = (
+  <div>
+    <Form horizontal>
+      <FormGroup controlId="formHorizontalEmail">
+        <Col componentClass={ControlLabel} sm={2}>
+          Email
+        </Col>
+        <Col sm={10}>
+          <FormControl type="email" placeholder="Email" />
+        </Col>
+      </FormGroup>
+
+      <FormGroup controlId="formHorizontalPassword">
+        <Col componentClass={ControlLabel} sm={2}>
+          Password
+        </Col>
+        <Col sm={10}>
+          <FormControl type="password" placeholder="Password" />
+        </Col>
+      </FormGroup>
+
+      <FormGroup>
+        <Col smOffset={2} sm={10}>
+          <Button type="submit">
+            Sign in
+          </Button>
+        </Col>
+      </FormGroup>
+    </Form>
+  </div>
+) ;
 
 class SettingsButton extends React.Component {
-  getInitialState() {
-    return { showModal: false };
-  }
-
-  close() {
-    this.setState({ showModal: false });
-  }
-
-  open() {
-    this.setState({ showModal: true });
-  }
-
-  save() {
-    //save data here then close modal
-    this.close
-  }
 
   render() {
       return (
         <div style={{float:'right'}}>
-          <img src="src/static/gear.png" style={settingsStyles} onClick={this.open}/>
 
-            <Modal show={this.state.showModal} onHide={this.close}>
-              <Modal.Header closeButton>
-                <Modal.Title>Settings</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>
-                <label for="host">OneOps Host:</label>
-                <input type="text" id="host"/>
-                <label for="token">API Token:</label>
-                <input type="text" id="token"/>
-              </Modal.Body>
-              <Modal.Footer>
-                <Button onClick={this.close}>Close</Button>
-                <Button bsStyle="primary" onClick={this.save}>Save</Button>
-              </Modal.Footer>
-            </Modal>
+
+            <ClusterToggleView
+              thumbViewStyle={thumbStyle}
+              thumbElement={thumb}
+              detailElement={formInstance} />
+
+
         </div>
       );
   }
