e9b55bd98b0e262c7d7216c9c6541be4e9e2aa57
jaredmharris <jharris@grassroots-tech.com>
2016-09-09 14:37:52 -0400

    styling updates

---
+++ src/app/components/configview.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import Button from 'react-bootstrap/lib/Button';
+import Col from 'react-bootstrap/lib/Col';
+import ControlLabel from 'react-bootstrap/lib/ControlLabel';
+import Form from 'react-bootstrap/lib/Form';
+import FormControl from 'react-bootstrap/lib/FormControl';
+import FormGroup from 'react-bootstrap/lib/FormGroup';
+import Glyphicon from 'react-bootstrap/lib/Glyphicon';
+import PageHeader from 'react-bootstrap/lib/PageHeader';
+
+const formStyle = {
+  paddingLeft: '90px',
+  paddingRight: '90px'
+}
+
+
+class ConfigView extends React.Component {
+
+  saveData() {
+    alert('Hello');
+  }
+
+
+  render() {
+    return(
+      <div >
+        <PageHeader style={{borderBottom: '1px solid #ddd', textAlign: 'center'}}>API Settings</PageHeader> <br/>
+        <Form horizontal style={formStyle}>
+          <FormGroup controlId="formHorizontalEmail">
+            <Col sm={2}>
+              OneOps Host:
+            </Col>
+            <Col sm={8}>
+              <FormControl type="text" placeholder="OneOps Host" />
+            </Col>
+          </FormGroup>
+<br/>
+          <FormGroup controlId="formHorizontalPassword">
+            <Col sm={2}>
+              API Token:
+            </Col>
+            <Col sm={8}>
+              <FormControl type="text" placeholder="API Token" />
+            </Col>
+          </FormGroup>
+<br/>
+          <FormGroup>
+            <Col smOffset={2} sm={10}>
+              <Button onClick={this.saveData}>
+                Save
+              </Button>
+            </Col>
+          </FormGroup>
+        </Form>
+      </div>
+    );
+  }
+
+}
+
+export default ConfigView;
--- src/app/components/settingsbutton.jsx
+++ src/app/components/settingsbutton.jsx
@@ -1,83 +1,34 @@
 import React from 'react';
 import ClusterToggleView from 'onesie-toggle-environment-block';
-import Button from 'react-bootstrap/lib/Button';
-import Col from 'react-bootstrap/lib/Col';
-import ControlLabel from 'react-bootstrap/lib/ControlLabel';
-import Form from 'react-bootstrap/lib/Form';
-import FormControl from 'react-bootstrap/lib/FormControl';
-import FormGroup from 'react-bootstrap/lib/FormGroup';
+import ConfigView from '../components/configview.jsx';
 
 
-const settingsStyles = {
-  verticalAlign: 'middle',
-  backgroundColor: '#f0f0f5',
-  height: '25',
-  width: '25'
-}
-
+const detailStyle = {
+  height: '20em',
+  width: '20em'
+};
 const thumbStyle = {
-  //margin: '.5em',
-  //padding: '.5em',
-  //width: '10em',
-  //display: 'flex',
-  //flexDirection: 'column',
-  //border: '1px solid white',
-  //borderRadius: '3px',
-  //boxShadow: '0 2px 2px rgba(0, 0, 0, .48)',
-  //overflow:'hidden',
-  minHeight: '3em',
-  width: '5em',
-  backgroundImage: 'url( src/static/gear.png )',
-  backgroundRepeat: 'no-repeat',
   backgroundColor: '#f0f0f5'
 };
+const imageStyle = {
+  margin: 'auto',
+  verticalAlign: 'middle',
+  height: '25px',
+  backgroundColor: '#f0f0f5'
+}
+const thumb = <img src="src/static/gear.png" style={imageStyle}/>;
 
-const thumb = <div></div>;
-const formInstance = (
-  <div>
-    <Form horizontal>
-      <FormGroup controlId="formHorizontalEmail">
-        <Col componentClass={ControlLabel} sm={2}>
-          Email
-        </Col>
-        <Col sm={10}>
-          <FormControl type="email" placeholder="Email" />
-        </Col>
-      </FormGroup>
-
-      <FormGroup controlId="formHorizontalPassword">
-        <Col componentClass={ControlLabel} sm={2}>
-          Password
-        </Col>
-        <Col sm={10}>
-          <FormControl type="password" placeholder="Password" />
-        </Col>
-      </FormGroup>
-
-      <FormGroup>
-        <Col smOffset={2} sm={10}>
-          <Button type="submit">
-            Sign in
-          </Button>
-        </Col>
-      </FormGroup>
-    </Form>
-  </div>
-) ;
 
 class SettingsButton extends React.Component {
 
   render() {
       return (
-        <div style={{float:'right'}}>
-
-
+        <div style={{float:'right', width:'30px'}}>
             <ClusterToggleView
               thumbViewStyle={thumbStyle}
+              detailViewStyle={detailStyle}
               thumbElement={thumb}
-              detailElement={formInstance} />
-
-
+              detailElement={<ConfigView />} />
         </div>
       );
   }
