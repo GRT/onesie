3d6a2c097143b5ba2483edc2b1442c5990b5e723
Oakley Hall <ohall@walmart.com>
2016-12-12 06:41:40 -0800

    eslint fix

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
 
 const footerStyles = {
-    height: '30px',
-    background: '#f0f0f5',
-    border: '1px solid white',
-    borderRadius: '3px',
-    overflow:'hidden',
-    position: 'fixed',
-    right: 0,
-    bottom: 0,
-    left: 0
+  height: '30px',
+  background: '#f0f0f5',
+  border: '1px solid white',
+  borderRadius: '3px',
+  overflow:'hidden',
+  position: 'fixed',
+  right: 0,
+  bottom: 0,
+  left: 0
 };
 
 
 class FooterComponent extends React.Component {
-
-    render() {
-        return (
-            <div style={footerStyles}>
-              {this.props.children}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div style={footerStyles}>
+        {this.props.children}
+      </div>
+    );
+  }
 }
+
+FooterComponent.propTypes = {
+  children: React.PropTypes.object.isRequired
+};
 
 export default FooterComponent;
